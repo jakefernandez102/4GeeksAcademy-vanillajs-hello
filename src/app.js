0ed4b71486511020078f5b39d3db26ec7376a080
Jake Fernandez B <jake.fernandezbrizuela@outlook.com>
2023-09-14 20:31:31 +0000

    Adding my solution

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+window.addEventListener("load", e => {
+  e.preventDefault();
+  generateExcuse();
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateExcuse = () => {
+  let excuse = "";
+  const excuseP = document.querySelector("#excuse");
+  let whoExcuse = Math.floor(Math.random() * who.length);
+  let actionExcuse = Math.floor(Math.random() * action.length);
+  let whatExcuse = Math.floor(Math.random() * what.length);
+  let whenExcuse = Math.floor(Math.random() * when.length);
+  console.log({ whoExcuse });
+  console.log({ actionExcuse });
+  console.log({ whatExcuse });
+  console.log({ whenExcuse });
+  excuse = `${who[whoExcuse]}  ${action[actionExcuse]}  ${what[whatExcuse]}  ${when[whenExcuse]}`;
+  excuseP.textContent = excuse;
 };
